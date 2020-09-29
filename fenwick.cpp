fa60752e9cc5f403ad99b2f399fde9eeafda01a8
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-09-29 10:35:39 +0700

    Update fenwick.cpp

--- fenwick.cpp
+++ fenwick.cpp
@@ -10,6 +10,11 @@ struct fenwick {
   template <typename IT>
   fenwick(IT begin, IT end) : n(end - begin) {
     tree.resize(n);
+    build(begin, end);
+  }
+  
+  template <typename IT>
+  void build(IT begin, IT end) {
     partial_sum(begin, end, tree.begin());
     for (int i = n - 1; i > 0; i--) {
       int k = (i & (i + 1)) - 1;
