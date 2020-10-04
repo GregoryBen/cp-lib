eeae82bd4c8508b44480d3c3d65f0603ec559139
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-10-04 21:57:18 +0700

    Update fenwick_range.cpp

--- fenwick_range.cpp
+++ fenwick_range.cpp
@@ -1,49 +1,46 @@
 |template <typename T>
 struct fenwick_range {
   int n;
-  vector<T> tree[2];
+  vector<T> data_mul, data_add;
 
   fenwick_range(int _n) : n(_n) {
     assert(n > 0);
-    tree[0].resize(n);
-    tree[1].resize(n);
+    data_mul.resize(n);
+    data_add.resize(n);
   }
 
   template <typename IT>
   fenwick_range(IT begin, IT end) : n(end - begin) {
-    tree[0].resize(n);
-    tree[1].resize(n);
+    data_mul.resize(n);
+    data_add.resize(n);
     for (int i = 0; i < n; i++) {
       update(i, i, *(begin++));
     }
   }
 
-  void update_bit(int b, int k, T x) {
+  void update_bit(int k, T mul, T add) {
     while (k < n) {
-      tree[b][k] += x;
+      data_mul[k] += mul;
+      data_add[k] += add;
       k |= (k + 1);
     }
   }
 
   void update(int l, int r, T x) {
     assert(0 <= l && l <= r && r <= n - 1);
-    update_bit(0, l, x);
-    update_bit(0, r + 1, -x);
-    update_bit(1, l, x * (l - 1));
-    update_bit(1, r + 1, -x * r);
-  }
-
-  T sum(int b, int k) {
-    T res{};
-    while (k >= 0) {
-      res += tree[b][k];
-      k = (k & (k + 1)) - 1;
-    }
-    return res;
+    update_bit(l, x, -x * (l - 1));
+    update_bit(r + 1, -x, x * r);
   }
 
   T query(int k) {
-    return sum(0, k) * k - sum(1, k);
+    T mul{}, add{};
+    T start = k;
+    while (k >= 0) {
+      mul += data_mul[k];
+      add += data_add[k];
+      k = (k & (k + 1)) - 1;
+    }
+    return mul * start + add;
   }
 
   T query(int l, int r) {
