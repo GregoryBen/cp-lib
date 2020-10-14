6fa7197fd4f55d422682dc979940adcceeb9df40
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-10-14 12:08:38 +0700

    Update stress.bat

--- stress.bat
+++ stress.bat
@@ -2,9 +2,9 @@
 set /a i = 1
 :loop
   echo %i%
-  gen.exe > in
-  check.exe < in > out1
-  brute.exe < in > out2
+  gen > in
+  check < in > out1
+  brute < in > out2
   fc out1 out2 > nul
   if errorlevel 1 goto fail
   set /a i = %i% + 1
