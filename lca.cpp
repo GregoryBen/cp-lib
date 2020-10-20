e70e2961464d217dffee4a3acd3fc1bbbe99e666
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-10-20 21:57:51 +0700

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,34 @@
+// O(n) preprocessing O(log n) per query
+// incomplete
+
+struct LCA {
+  int n;
+  vector<int> height, first, euler, segtree;
+  vector<bool> vis;
+
+  LCA(vector<vector<int>>& g, int root = 0) : n(g.size()) {
+    height.resize(n);
+    first.resize(n);
+    euler.resize(2 * n);
+    segtree.resize(8 * n);
+    vis.resize(n);
+    dfs(g, root);
+    build(1, 0, m - 1); // ??
+  }
+
+  void dfs(vector<vector<int>>& g, int v, int h = 0) {
+    vis[v] = true;
+    height[v] = h;
+    first[node] = euler.size();
+    euler.emplace_back(v);
+    for (int i : g[v]) {
+      if (vis[i]) continue;
+      dfs(g, i, h + 1);
+      euler.emplace_back(v);
+    }
+  }
+
+  void build(int v, int b, int e) {
+
+  }
+};
