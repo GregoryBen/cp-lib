2a79483d4f298db012058f08d471dd140a80dfb8
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-10-28 18:10:41 +0700

    Update centroid.cpp

--- centroid.cpp
+++ centroid.cpp
@@ -24,3 +24,32 @@ vector<int> find_centroid(const vector<vector<int>>& g) {
   assert(cen.size() <= 2);
   return cen;
 }
+
+// find one centroid with death node/edge
+
+int one_centroid(int root, const vector<vector<int>>& g, const vector<bool>& dead) {
+  assert(!g.empty());
+  vector<int> sz(g.size());
+  function<void(int, int)> dfs_sz = [&](int v, int p) {
+    sz[v] = 1;
+    for (int i : g[v]) {
+      if (i != p && !dead[i]) {
+        dfs_sz(i, v);
+        sz[v] += sz[i];
+      }
+    }
+  };
+  dfs_sz(root, -1);
+  int n = sz[root];
+  function<int(int, int)> dfs = [&](int v, int p) {
+    for (int i : g[v]) {
+      if (i != p && !dead[i]) {
+        if (sz[i] > n / 2) {
+          return dfs(i, v);
+        }
+      }
+    }
+    return v;
+  };
+  return dfs(root, -1);
+}
