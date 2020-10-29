e6e80325afb912877bec8be53bb3e5d6b2053b80
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-10-29 21:19:45 +0700

    Update fast_IO.cpp

--- fast_IO.cpp
+++ fast_IO.cpp
@@ -5,22 +5,22 @@ static int write_pos = 0;
 static char write_buf[BUF_SIZE];
 
 inline char get_char() {
-	static char buf[BUF_SIZE];
-	static int len = 0, pos = 0;
-	if (pos == len) {
-		pos = 0, len = fread(buf, 1, BUF_SIZE, stdin);
-	}
-	if (pos == len) {
-		return -1;
-	}
-	return buf[pos++];
+  static char buf[BUF_SIZE];
+  static int len = 0, pos = 0;
+  if (pos == len) {
+    pos = 0, len = fread(buf, 1, BUF_SIZE, stdin);
+  }
+  if (pos == len) {
+    return -1;
+  }
+  return buf[pos++];
 }
 
 inline char scan_char() {
-	char c = get_char();
-	while (c <= 32)
-		c = get_char();
-	return c;
+  char c = get_char();
+  while (c <= 32)
+    c = get_char();
+  return c;
 }
 
 inline string scan_str() {
@@ -38,61 +38,62 @@ inline string scan_str() {
 
 template <typename T = int>
 inline T scan() {
-	int s = 1;
-	char c = scan_char();
-	T x = 0;
-	if (c == '-') {
-		s = -1, c = get_char();
-	}
-	while ('0' <= c && c <= '9') {
-		x = x * 10 + c - '0';
-		c = get_char();
-	}
-	return s == 1 ? x : -x;
+  int s = 1;
+  char c = scan_char();
+  T x = 0;
+  if (c == '-') {
+    s = -1, c = get_char();
+  }
+  while ('0' <= c && c <= '9') {
+    x = x * 10 + c - '0';
+    c = get_char();
+  }
+  return s == 1 ? x : -x;
 }
 
 inline void write_char(char x) {
-	if (write_pos == BUF_SIZE) {
-		fwrite(write_buf, 1, BUF_SIZE, stdout);
-		write_pos = 0;
-	}
-	write_buf[write_pos++] = x;
+  if (write_pos == BUF_SIZE) {
+    fwrite(write_buf, 1, BUF_SIZE, stdout);
+    write_pos = 0;
+  }
+  write_buf[write_pos++] = x;
 }
 
-inline void write_str(string s, char end = 0) {
-	for (int i = 0; i < (int) s.size(); i++) {
-		write_char(s[i]);
-	}
-	if (end) {
+inline void write_str(string str, char end = 0) {
+  for (char c : str) {
+    write_char(c);
+  }
+  if (end) {
     write_char(end);
-	}
+  }
 }
 
 template <typename T>
 inline void write(T x, char end = 0) {
-	if (x < 0) {
-		write_char('-'), x = -x;
-	}
-	int n = 0;
-	char s[24];
-	while (x || !n) {
-		s[n++] = '0' + x % 10, x /= 10;
-	}
-	while (n--) {
-		write_char(s[n]);
-	}
-	if (end) {
-		write_char(end);
-	}
+  if (x < 0) {
+    write_char('-'), x = -x;
+  }
+  int n = 0;
+  char s[24];
+  while (x || !n) {
+    s[n++] = '0' + x % 10;
+    x /= 10;
+  }
+  while (n--) {
+    write_char(s[n]);
+  }
+  if (end) {
+    write_char(end);
+  }
 }
 
 struct Flusher {
-	~Flusher() {
-		if (write_pos) {
-			fwrite(write_buf, 1, write_pos, stdout);
-			write_pos = 0;
-		}
-	}
+  ~Flusher() {
+    if (write_pos) {
+      fwrite(write_buf, 1, write_pos, stdout);
+      write_pos = 0;
+    }
+  }
 } flusher;
 
 } // namespace fast_IO
