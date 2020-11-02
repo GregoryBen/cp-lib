b3c22318e3f80d2d6a40ccb1f4cf6a796ffbc0cd
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-02 21:23:43 +0700

    Update hld.cpp

--- hld.cpp
+++ hld.cpp
@@ -2,58 +2,65 @@
 // hld 0 index and subtree update
 
 template <typename T, typename L>
-struct HeavyLight : segtree<T, L> {
+struct HeavyLight : public segtree<T, L> {
   using segtree<T, L>::n;
   using segtree<T, L>::unite;
   using segtree<T, L>::update;
   using segtree<T, L>::query;
 
+  int timer;
   bool is_node;
-  vector<int> par;
-  vector<int> heavy;
-  vector<int> sz;
-  vector<int> depth;
-  vector<int> root;
   vector<int> pos;
+  vector<int> par;
+  vector<int> root;
+  vector<int> depth;
+  vector<int> sz;
+  vector<int> rpos;
+  vector<vector<int>> g;
 
-  template <typename G>
-  void dfs(const G& g, int v) {
-    int max_subtree = 0;
+  void dfs_sz(int v) {
     sz[v] = 1;
     for (int i : g[v]) {
-      if (i != par[v]) {
-        par[i] = v;
-        depth[i] = depth[v] + 1;
-        dfs(g, i);
-        if (sz[i] > max_subtree) {
-          heavy[v] = i;
-          max_subtree = sz[i];
-        }
-        sz[v] += sz[i];
+      par[i] = v;
+      depth[i] = depth[v] + 1;
+      g[i].erase(find(g[i].begin(), g[i].end(), v));
+      dfs_sz(i);
+      sz[v] += sz[i];
+      if (sz[i] > sz[g[v][0]]) {
+        swap(i, g[v][0]);
       }
     }
   }
 
-  void decompose() {
-    int cur_pos = 0;
-    for (int i = 0; i < n; i++) {
-      if (par[i] == -1 || heavy[par[i]] != i) {
-        for (int j = i; j != -1; j = heavy[j]) {
-          root[j] = i;
-          pos[j] = cur_pos++;
-        }
-      }
+  void dfs_hld(int v) {
+    pos[v] = timer++;
+    rpos.emplace_back(v);
+    for (int i : g[v]) {
+      root[i] = (i == g[v][0] ? root[v] : i);
+      dfs_hld(i);
     }
   }
+
+  int lca(int u, int v) {
+		for(; root[u] != root[v]; v = par[root[v]]) {
+      if(depth[root[u]] > depth[root[v]]) {
+        swap(u, v);
+      }
+		}
+		return depth[u] < depth[v] ? u : v;
+  }
+
+  int dist(int u, int v, int w = -1) {
+    return depth[u] + depth[v] - 2 * depth[~w ? w : lca(u, v)];
+  }
+
 
   void process_path(int u, int v, function<void(int, int)> f) {
     for (; root[u] != root[v]; v = par[root[v]]) {
       if (depth[root[u]] > depth[root[v]]) {
         swap(u, v);
       }
-      cerr << pos[root[v]] << ' ' << pos[v] << '\n';
       f(pos[root[v]], pos[v]);
-      cerr << "done" << '\n';
     }
     if (depth[u] > depth[v]) {
       swap(u, v);
@@ -62,21 +69,25 @@ struct HeavyLight : segtree<T, L> {
   }
 
   template <typename G>
-  HeavyLight(const G& g, bool _is_node) : segtree<T, L>(g.size()), is_node(_is_node) {
+  HeavyLight(const G& _g, bool _is_node, int r = 0) : segtree<T, L>(_g.size()) {
     assert(n > 0);
-    par.resize(n, -1);
-    heavy.resize(n, -1);
-    sz.resize(n);
-    depth.resize(n);
-    root.resize(n);
+    timer = 0;
+    is_node = _is_node;
     pos.resize(n);
-    dfs(g, 0);
-    decompose();
-    }
+    par.resize(n, -1);
+    root.resize(n, r);
+    depth.resize(n);
+    sz.resize(n);
+    rpos.resize(n);
+    g = _g;
+    dfs_sz(r);
+    root[r] = r;
+    dfs_hld(r);
+  }
 
-//  void update_subtree(int v, const L& x) { // (?)
-//    update(pos[v] + (is_node ? 0 : 1), pos[v] + sz[v] - 1, x);
-//  }
+  void update_subtree(int v, const L& x) {
+    update(pos[v] + (is_node ? 0 : 1), pos[v] + sz[v] - 1, x);
+  }
 
   void update_path(int u, int v, const L& x) {
     process_path(u, v, [this, &x](int l, int r) {
