4b6d3e59af5e84a836fad99f879e6459bc6749d8
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-02 11:50:18 +0700

    Update hld.cpp

--- hld.cpp
+++ hld.cpp
@@ -1,26 +1,30 @@
 // incomplete
+// hld 0 index and subtree update
 
-template <typename T>
-struct HeavyLight {
-  int n;
-  bool edge_val;
+template <typename T, typename L>
+struct HeavyLight : segtree<T, L> {
+  using segtree<T, L>::n;
+  using segtree<T, L>::unite;
+  using segtree<T, L>::update;
+  using segtree<T, L>::query;
+
+  bool is_node;
   vector<int> par;
   vector<int> heavy;
   vector<int> sz;
   vector<int> depth;
   vector<int> root;
   vector<int> pos;
-  segtree<T, T> tree;
 
   template <typename G>
-  void dfs(const vector<vector<int>>& g, int v) {
-    int sz[v] = 1;
+  void dfs(const G& g, int v) {
     int max_subtree = 0;
+    sz[v] = 1;
     for (int i : g[v]) {
       if (i != par[v]) {
         par[i] = v;
         depth[i] = depth[v] + 1;
-        int sz[i] = dfs(g, i);
+        dfs(g, i);
         if (sz[i] > max_subtree) {
           heavy[v] = i;
           max_subtree = sz[i];
@@ -28,19 +32,6 @@ struct HeavyLight {
         sz[v] += sz[i];
       }
     }
-  }
-
-  void process_path(int u, int v, function<void(int, int)> f) {
-    for (; root[u] != root[v]; v = par[root[v]]) {
-      if (depth[root[u]] > depth[root[v]]) {
-        swap(u, v);
-      }
-      f(pos[root[v]], pos[v] + 1);
-    }
-    if (depth[u] > depth[v]) {
-      swap(u, v);
-    }
-    f(pos[u] + (edge_val ? 0 : 1), pos[v]);
   }
 
   void decompose() {
@@ -55,32 +46,49 @@ struct HeavyLight {
     }
   }
 
+  void process_path(int u, int v, function<void(int, int)> f) {
+    for (; root[u] != root[v]; v = par[root[v]]) {
+      if (depth[root[u]] > depth[root[v]]) {
+        swap(u, v);
+      }
+      cerr << pos[root[v]] << ' ' << pos[v] << '\n';
+      f(pos[root[v]], pos[v]);
+      cerr << "done" << '\n';
+    }
+    if (depth[u] > depth[v]) {
+      swap(u, v);
+    }
+    f(pos[u] + (is_node ? 0 : 1), pos[v]);
+  }
+
   template <typename G>
-  HeavyLight(const G& g, bool _edge_val) : n(g.size()), edge_val(_edge_val) {
+  HeavyLight(const G& g, bool _is_node) : segtree<T, L>(g.size()), is_node(_is_node) {
     assert(n > 0);
-    heavy.resize(n, -1);
     par.resize(n, -1);
+    heavy.resize(n, -1);
+    sz.resize(n);
     depth.resize(n);
     root.resize(n);
     pos.resize(n);
     dfs(g, 0);
     decompose();
-  }
+    }
 
-  void modify_subtree(int v, int x) {
-    tree.update(pos[v] + (edge_val ? 0 : 1), pos[v] + sz[v] - 1, v);
-  }
+//  void update_subtree(int v, const L& x) { // (?)
+//    update(pos[v] + (is_node ? 0 : 1), pos[v] + sz[v] - 1, x);
+//  }
 
-  void modify_path(int u, int v, const T& x) {
+  void update_path(int u, int v, const L& x) {
     process_path(u, v, [this, &x](int l, int r) {
-      tree.update(l, r, x);
+      update(l, r, x);
     });
   }
 
-  void query(int u, int v) {
+  T query_path(int u, int v) {
     T res{};
     process_path(u, v, [this, &res](int l, int r) {
-      res.unite(res, tree.query(l, r));
+      unite(res, query(l, r));
     });
+    return res;
   }
 };
