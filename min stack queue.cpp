27c7d36b1f7669e8bfdd4e1f0dda03b0fcf12e1b
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-03 21:50:16 +0700

    Create min stack queue.cpp

---
+++ min stack queue.cpp
@@ -0,0 +1,30 @@
+// min stack
+// all function O(1)
+// change to 'max' for max_stack in push function
+
+template <typename T>
+struct min_stack {
+  struct node {
+    T cur;
+    T min;
+  };
+
+  stack<node> st;
+
+  void push(T x) {
+    T y = st.empty() ? x : min(x, st.top().min);
+    st.push({x, y});
+  }
+
+  void pop() {
+    st.pop();
+  }
+
+  node top() {
+    return st.top();
+  }
+};
+
+// min queue
+// all function O(1)
+
