962602cc07d535f6ce676ac7974cdbd9b6840605
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-03 22:15:48 +0700

    Update min stack queue.cpp

--- min stack queue.cpp
+++ min stack queue.cpp
@@ -25,3 +25,23 @@ struct min_stack {
 // min queue
 // all function O(1)
 
+template<typename T> 
+struct min_queue {
+  deque<T> D, Q;
+
+  void push(T x) {
+    while (!D.empty() && x < D.back()) D.pop_back(); // Change to '>' for max_queue
+    D.push_back(x);
+    Q.push_back(x);
+  }
+
+  void pop() {
+    if (D.front() == Q.front()) D.pop_front();
+    Q.pop_front();
+  }
+
+  T top()   { return D.front(); }
+  T front() { return Q.front(); }
+  T empty() { return Q.empty(); }
+  T size()  { return Q.size();  }
+};
