8f54b43712e51bbe872a0f1dd91f9ee9b1529f91
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-03 10:48:34 +0700

    Update hld.cpp

--- hld.cpp
+++ hld.cpp
@@ -42,12 +42,12 @@ struct HeavyLight : public segtree<T, L> {
   }
 
   int lca(int u, int v) {
-    for(; root[u] != root[v]; v = par[root[v]]) {
+		for(; root[u] != root[v]; v = par[root[v]]) {
       if(depth[root[u]] > depth[root[v]]) {
         swap(u, v);
       }
-    }
-    return depth[u] < depth[v] ? u : v;
+		}
+		return depth[u] < depth[v] ? u : v;
   }
 
   int dist(int u, int v, int w = -1) {
@@ -98,7 +98,7 @@ struct HeavyLight : public segtree<T, L> {
   T query_path(int u, int v) {
     T res{};
     process_path(u, v, [this, &res](int l, int r) {
-      unite(res, query(l, r));
+      res = unite(res, query(l, r));
     });
     return res;
   }
