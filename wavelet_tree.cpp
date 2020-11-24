73e3256d563b953f562ae0192748c6a9b448ba4b
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-24 13:16:38 +0700

    Update wavelet_tree.cpp

--- wavelet_tree.cpp
+++ wavelet_tree.cpp
@@ -1,26 +1,25 @@
 // preprocessing : O(log n log(max(A))
 // query : (log(max(A))
-// max(A) :  maximum element in array
-// ex comp : [&](int i, int j) { return i < j; };
+// max(A) : maximum element in array
+// ex comp : [&](int i, int j) { return i < j; }; (default)
 
 template <typename T>
 struct wavelet_node {
-  int n;
   T lo, hi;
   wavelet_node *l = nullptr;
   wavelet_node *r = nullptr;
   vector<int> b;
 
   template <typename IT, class C>
-  wavelet_node(IT begin, IT end, T _lo, T _hi, const C& comp) : n(end - begin), lo(_lo), hi(_hi) {
-    if(n == 0 || lo == hi) {
+  wavelet_node(IT begin, IT end, T _lo, T _hi, const C& comp) : lo(_lo), hi(_hi) {
+    if(begin >= end || lo == hi) {
       return;
     }
     T mid = lo + (hi - lo) / 2;
     auto f = [&](T x) {
-      return comp(x, mid);
+      return comp(x, mid + 1);
     };
-    b.reserve(n + 1);
+    b.reserve(end - begin + 1);
     b.emplace_back(0);
     for (auto it = begin; it != end; it++) {
       b.emplace_back(b.back() + f(*it));
@@ -34,46 +33,69 @@ struct wavelet_node {
 template <typename T, class C = function<bool(const T&, const T&)>>
 struct wavelet_tree {
   int n;
+  T mn, mx;
   wavelet_node<T> *root;
   C comp;
 
   template <typename IT>
-  wavelet_tree(IT begin, IT end, const C& c) : n(end - begin), comp(c) {
+  wavelet_tree(IT begin, IT end, const C& c = [&](T i, T j) { return i < j; }) : n(end - begin), comp(c) {
     assert(n > 0);
-    T min = *min_element(begin, end, comp);
-    T max = *max_element(begin, end, comp);
-    root = new wavelet_node<T>(begin, end, min, max, comp);
+    mn = *min_element(begin, end, comp);
+    mx = *max_element(begin, end, comp);
+    root = new wavelet_node<T>(begin, end, mn, mx, comp);
   }
 
   T kth(wavelet_node<T> *v, int l, int r, int k) {
-    assert(0 <= k && k < v->n);
     if (v->lo == v->hi) {
       return v->lo;
     }
-    int lb = v->b[l];
+    int lb = v->b[l - 1];
     int rb = v->b[r];
     int inLeft = rb - lb;
-    return k <= inLeft ? kth(v->l, lb, rb, k) : kth(v->r, l - lb, r - rb, k - inLeft);
+    return k <= inLeft ? kth(v->l, lb + 1, rb, k) : kth(v->r, l - lb, r - rb, k - inLeft);
   }
 
   int LTE(wavelet_node<T> *v, int l, int r, T x) {
     if (l > r || !comp(v->lo, x)) {
       return 0;
     }
-    if (!comp(x, v->hi)) {
+    if (!comp(x, v->hi + 1)) {
       return r - l + 1;
     }
-    int lb = v->b[l];
+    int lb = v->b[l - 1];
     int rb = v->b[r];
-    return LTE(v->l, lb, rb, x) + LTE(v->r, l - lb, r - rb, x);
+    return LTE(v->l, lb + 1, rb, x) + LTE(v->r, l - lb, r - rb, x);
+  }
+
+  int count(wavelet_node<T> *v, int l, int r, T x) {
+    if (l > r || comp(x, v->lo) || comp(v->hi, x)) {
+      return 0;
+    }
+    if (v->lo == v->hi) {
+      return r - l + 1;
+    }
+    int lb = v->b[l - 1];
+    int rb = v->b[r];
+    T mid = v->lo + (v->hi - v->lo) / 2;
+    return x <= mid ? count(v->l, lb + 1, rb, x) : count(v->r, l - lb, r - rb, x);
   }
 
   T kth(int l, int r, int k) {
+    assert(0 <= l && l <= r && r <= n - 1 && 0 <= k && k <= n - 1);
+    ++l; ++r; ++k;
     return kth(root, l, r, k);
   }
 
   int LTE(int l, int r, T x) {
+    assert(0 <= l && l <= r && r <= n - 1 && mn <= x && x <= mx);
+    ++l; ++r;
     return LTE(root, l, r, x);
+  }
+
+  int count(int l, int r, T x) {
+    assert(0 <= l && l <= r && r <= n - 1 && mn <= x && x <= mx);
+    ++l; ++r;
+    return count(root, l, r, x);
   }
 
   void debug_tree() {
@@ -86,11 +108,11 @@ struct wavelet_tree {
         for (auto x : v->b) {
           cerr << x << " ";
         }
-        cerr << "\n";
-        if (v->l) {
+        cerr << '\n';
+        if (v->l != nullptr) {
           d.emplace_back(v->l);
         }
-        if (v->r) {
+        if (v->r != nullptr) {
           d.emplace_back(v->r);
         }
       }
