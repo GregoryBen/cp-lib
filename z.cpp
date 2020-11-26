ed2effb27eb09471e10616dc888ded3ef131f3c9
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-26 13:52:52 +0700

    Create z.cpp

---
+++ z.cpp
@@ -0,0 +1,18 @@
+// credit : CPH
+// O(n)
+
+template <typename T>
+vector<int> z_function(const T &s) {
+  int n = s.size();
+  vector<int> z(n);
+  int x = 0, y = 0;
+  for (int i = 1; i < n; i++) {
+    z[i] = max(0, min(z[i - x], y - i + 1));
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      x = i;
+      y = i + z[i];
+      z[i]++;
+    }
+  }
+  return z;
+}
