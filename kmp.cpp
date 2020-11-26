f407ec49aa55f1c7001d4e8eaeb91fb8c9d1fbcd
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-26 15:45:02 +0700

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -16,3 +16,19 @@ vector<int> prefix_function(const T &s){
   }
   return p;
 }
+
+template <typename T>
+vector<int> getZ(const T &s) {
+  int n = (int) s.size();
+  vector<int> z(n);
+  vector<int> p = prefix_function(s);
+  for (int i = 0; i < n - 1; i++) {
+    debug(i, i - p[i] + 1);
+    z[i - p[i] + 1] = max(z[i - p[i] + 1], p[i]);
+  }
+  debug("here");
+  for (int i = 1; i < n; i++) {
+    z[i] = max(z[i - 1] - 1, z[i]);
+  }
+  return z;
+}
