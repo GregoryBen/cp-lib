23a7d21303f0cf1b805220fba9b01420973a8f48
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-26 15:05:35 +0700

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -3,16 +3,16 @@
 
 template <typename T>
 vector<int> prefix_function(const T &s){
-	int n = (int) s.size(), j = 0;
-	vector<int> p(n);
-	for(int i = 1; i < n; i++) {
-		while (j > 0 && s[i] != s[j]) {
+  int n = (int) s.size(), j = 0;
+  vector<int> p(n);
+  for(int i = 1; i < n; i++) {
+    while (j > 0 && s[i] != s[j]) {
       j = p[j - 1];
-		}
-		if(s[i] == s[j]) {
+    }
+    if(s[i] == s[j]) {
       j++;
-		}
-		p[i] = j;
-	}
-	return p;
+    }
+    p[i] = j;
+  }
+  return p;
 }
