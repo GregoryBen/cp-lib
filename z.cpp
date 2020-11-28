32dff03165bdb9c7db6f1634bf3d246dc9bf298b
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-28 22:57:29 +0700

    Update z.cpp

--- z.cpp
+++ z.cpp
@@ -8,6 +8,7 @@ vector<int> z_function(const T &s) {
   int x = 0, y = 0;
   for (int i = 1; i < n; i++) {
     z[i] = max(0, min(z[i - x], y - i + 1));
+    if (i + z[i] < y) continue;
     while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
       x = i;
       y = i + z[i];
