68a989b5a8c37428949b399064c9a627ddb62ef7
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-28 22:21:51 +0700

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -2,7 +2,7 @@
 // O(n)
 
 template <typename T>
-vector<int> prefix_function(const T &s){
+vector<int> prefix_function(const T &s) {
   int n = (int) s.size(), j = 0;
   vector<int> p(n);
   for(int i = 1; i < n; i++) {
