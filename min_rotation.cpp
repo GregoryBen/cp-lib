81458fa0a58821631df22be92fd5c523a96d3250
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-11-29 11:37:53 +0700

    Update min_rotation.cpp

--- min_rotation.cpp
+++ min_rotation.cpp
@@ -1,9 +1,12 @@
 // O(n)
 // rotate(v.begin(), v.begin() + least_rotation(v), v.end())
 
-int least_rotation(string s) {
+template <typename T>
+int least_rotation(T s) {
   int n = (int) s.size(), k = 0;
-  s += s;
+  for (int i = 0; i < n; i++) {
+    s.push_back(s[i]);
+  }
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       if (k + j == i || s[k + j] < s[i + j]) {
@@ -18,4 +21,3 @@ int least_rotation(string s) {
   }
   return k;
 }
- 
