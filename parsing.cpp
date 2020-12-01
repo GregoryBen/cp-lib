efd864b8d05031393854297f26afe5a1eaab08c1
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-01 12:01:41 +0700

    Update parsing.cpp

--- parsing.cpp
+++ parsing.cpp
@@ -1,62 +1,67 @@
+// O(n)
+
 int num_eval;
 stack<int> values;
 stack<char> ops;
 
 void process_op() {
-    int r = values.top();
-    values.pop();
-    int l = values.top();
-    values.pop();
-    char op = ops.top();
-    ops.pop();
-    switch (op) {
-        case '+':
-            values.push(l + r);
-            break;
-        case '-':
-            values.push(l - r);
-            break;
-        case '*':
-            values.push(l * r);
-            break;
-        case '/':
-            values.push(l / r);
-            break;
-    }
+  int r = values.top();
+  values.pop();
+  int l = values.top();
+  values.pop();
+  char op = ops.top();
+  ops.pop();
+  switch (op) {
+    case '+':
+      values.push(l + r);
+      break;
+    case '-':
+      values.push(l - r);
+      break;
+    case '*':
+      values.push(l * r);
+      break;
+    case '/':
+      values.push(l / r);
+      break;
+  }
 }
 
 int priority(char op) {
-    switch (op) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-    }
-    return 0;
+  switch (op) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+  }
+  return 0;
 }
 
 int eval(string s) {
-    num_eval++;
-    s = '(' + s + ')';
-    for (char c : s)
-        if (c == ' ')
-          continue;
-        else if ('0' <= c && c <= '9')
-            values.push(c - '0');
-        else if (c == '(')
-            ops.push(c);
-        else if (c == ')') {
-            while (ops.top() != '(')
-                process_op();
-            ops.pop();
-        } else {
-            while (!ops.empty() && priority(ops.top()) >= priority(c))
-                process_op();
-            ops.push(c);
-        }
-    int ans = values.top();
-    values.pop();
-    return ans;
+  num_eval++;
+  s = '(' + s + ')';
+  for (char c : s) {
+    if (c == ' ') {
+      continue;
+    } else if ('0' <= c && c <= '9') {
+      values.push(c - '0');
+    } else if (c == '(') {
+      ops.push(c);
+    } else if (c == ')') {
+      while (ops.top() != '(') {
+        process_op();
+      }
+      ops.pop();
+    } else {
+      while (!ops.empty() && priority(ops.top()) >= priority(c)) {
+        process_op();
+      }
+      ops.push(c);
+    }
+  }
+  int ans = values.top();
+  values.pop();
+  return ans;
 }
