65e6a2f4a1e6ff1a8d452b435f76f704e66fce65
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-07 11:58:20 +0700

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -51,11 +51,11 @@ vector<vector<int>> prefix_automaton(const string &s, const vector<int> &p) {
   int n = (int) s.size();
   vector<vector<int>> aut(n, vector<int>(26));
   for (int i = 0; i < n; i++) {
-    for (char c = 'a'; c <= 'z'; c++) {
-      if (i > 0 && c != s[i]) {
+    for (int c = 0; c < 26; c++) {
+      if (i > 0 && c + 'a' != s[i]) {
         aut[i][c] = aut[p[i - 1]][c];
       } else {
-        aut[i][c] = i + (c == s[i]);
+        aut[i][c] = i + (c + 'a' == s[i]);
       }
     }
   }
