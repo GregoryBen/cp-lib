fdcf690b741076b28bc1422ceea0a40a47461d5d
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-11 16:48:23 +0700

    Create suffix_automaton.cpp

---
+++ suffix_automaton.cpp
@@ -0,0 +1,62 @@
+template <typename T>
+struct suffix_automaton {
+  struct node {
+    int first_pos = -1;
+
+    int len;
+    int link;
+    vector<int> inv_link;
+    vector<int> next;
+
+    node() {
+      next.resize(128, -1);
+    }
+  };
+
+  int sz;
+  int last;
+  vector<node> st;
+
+  suffix_automaton(const T &s) {
+    assert(!s.empty());
+    sz = 1;
+    last = 0;
+    st.emplace_back();
+    st[0].link = -1;
+    for (int i = 0; i < (int) s.size(); i++) {
+      int c = s[i];
+      int cur = sz++;
+      st.emplace_back();
+      st[cur].len = i + 1;
+      st[cur].first_pos = i;
+      int p = last;
+      for (; p != -1 && st[p].next[c] == -1; p = st[p].link) {
+        st[p].next[c] = cur; // (?)
+      }
+      if (p == -1) {
+        st[cur].link = 0;
+      } else {
+        int q = st[p].next[c];
+        if (st[p].len + 1 == st[q].len) {
+          st[cur].link = q;
+        } else {
+          int clone = sz++;
+          st.emplace_back();
+          st[clone].len = st[p].len + 1;
+          st[clone].first_pos = st[q].first_pos;
+          st[clone].next = st[q].next;
+          st[clone].link = st[q].link;
+          for(; p != -1 && st[p].next[c] == q; p = st[p].link) {
+            st[p].next[c] = clone;
+          }
+          st[q].link = clone;
+          st[cur].link = clone;
+        }
+      }
+      last = cur;
+    }
+    for (int i = 0; i < sz; i++) {
+      st[st[i].link].inv_link.emplace_back(i);
+    }
+  }
+};
