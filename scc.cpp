e802a0e5812163a61b50b8796653a3043a281391
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-16 18:13:17 +0700

    Update and rename tarjan_scc.cpp to scc.cpp

--- scc.cpp
+++ scc.cpp
@@ -1,4 +1,46 @@
-vector<int> tarjan_scc(const vector<vector<int>> &g, int &cnt) {
+// kosaraju
+
+vector<int> get_scc(const vector<vector<int>> &g, const vector<vector<int>> &gr, int &cnt) {
+  int n = g.size();
+  vector<bool> vis(n);
+  vector<int> topo;
+  function<void(int)> dfs = [&](int i) {
+    vis[i] = true;
+    for (int j : g[i]) {
+      if (!vis[j]) {
+        dfs(j);
+      }
+    }
+    topo.emplace_back(i);
+  };
+  for (int i = 0; i < n; i++) {
+    if (!vis[i]) {
+      dfs(i);
+    }
+  }
+  vector<int> c(n, -1);
+  function<void(int)> dfs1 = [&](int i) {
+    for (int j : gr[i]) {
+      if (c[j] == -1) {
+        c[j] = c[i];
+        dfs1(j);
+      }
+    }
+  };
+  for (int i = n - 1; i >= 0; i--) {
+    int k = topo[i];
+    if (c[k] != -1) {
+      continue;
+    }
+    c[k] = cnt++;
+    dfs1(k);
+  }
+  return c;
+}
+
+// tarjan
+
+vector<int> get_scc(const vector<vector<int>> &g, int &cnt) {
   int n = g.size();
   int time = 0;
   vector<int> c(n);
