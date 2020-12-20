843c9b2279d343b8a65ea664addff49acfeba337
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-20 19:22:02 +0700

    Update euler_path.cpp

--- euler_path.cpp
+++ euler_path.cpp
@@ -36,3 +36,35 @@ vector<int> euler_walk(const vector<vector<pair<int, int>>> &g, int m, int sc = 
   }
   return vector<int>(res.rbegin(), res.rend());
 }
+
+// more strightforward, but destroy graph
+
+vector<int> euler_walk(vector<vector<pair<int, int>>> &g, int m, int sc = 0) {
+  bool bad = false;
+  vector<int> vis(m);
+  vector<pair<int, int>> res;
+  function<void(int, int)> dfs = [&](int p, int i) {
+    while (!g[i].empty()) {
+      auto [j, e] = g[i].back();
+      g[i].pop_back();
+      if (!vis[e]) {
+        vis[e] = true;
+        dfs(i, j);
+      }
+    }
+    if (!res.empty() && res.back().first != i) {
+      bad = true;
+    }
+    res.emplace_back(p, i);
+  };
+  dfs(-1, sc);
+  if ((int) res.size() != m + 1 || bad) {
+    return vector<int>();
+  }
+  debug(res);
+  vector<int> ans(m + 1);
+  for (int i = m; i >= 0; i--) {
+    ans[m - i] = res[i].second;
+  }
+  return ans;
+}
