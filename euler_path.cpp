f45692ad2341bc0a3e5fea788652f067c9520eb5
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-20 17:01:08 +0700

    Update euler_path.cpp

--- euler_path.cpp
+++ euler_path.cpp
@@ -1,34 +1,35 @@
 // author : Simon Lindholm
+// deg = indegree - outdegree
 
 vector<int> euler_walk(const vector<vector<pair<int, int>>> &g, int m, int sc = 0) {
   int n = g.size();
-  vector<int> D(n);
-  vector<int> its(n);
-  vector<int> eu(m);
+  vector<int> deg(n);
+  vector<int> at(n);
+  vector<bool> vis(m);
   vector<int> res;
   stack<int> s;
   s.push(sc);
-  D[sc]++;
+  deg[sc]++;
   while (!s.empty()) {
     int x = s.top();
-    if (its[x] == (int) g[x].size()) {
+    if (at[x] == (int) g[x].size()) {
       res.push_back(x);
       s.pop();
       continue;
     }
     int y, e;
-    tie(y, e) = g[x][its[x]++];
-    if (!eu[e]) {
-      D[x]--;
-      D[y]++;
-      eu[e] = 1;
+    tie(y, e) = g[x][at[x]++];
+    if (!vis[e]) {
+      deg[x]--;
+      deg[y]++;
+      vis[e] = true;
       s.push(y);
     }
   }
   if ((int) res.size() != m + 1) {
     return vector<int>();
   }
-  for (int x : D) {
+  for (int x : deg) {
     if (x < 0) {
       return vector<int>();
     }
