1db8f294ddfb41f2560ff6fe8d85e095d7bb59db
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-25 15:22:38 +0700

    Update gomory_hu.cpp

--- gomory_hu.cpp
+++ gomory_hu.cpp
@@ -5,7 +5,7 @@ vector<vector<pair<int, int>>> gomory_hu(dinic<long long, false> &g) {
   int n = g.n;
   vector<vector<pair<int, int>>> t(n);
   vector<int> p(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 1; i < n; i++) {
     g.clear_flow();
     int f = g.max_flow(i, p[i]);
     vector<bool> cut = g.min_cut();
