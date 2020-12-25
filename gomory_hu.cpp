28274f79b5734aa94f8338b498e925090b5bb923
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-25 15:15:09 +0700

    Create gomory_hu.cpp

---
+++ gomory_hu.cpp
@@ -0,0 +1,20 @@
+// O(n) call dinic's max_flow
+
+template <typename T, bool S>
+vector<vector<pair<int, int>>> gomory_hu(dinic<long long, false> &g) {
+  int n = g.n;
+  vector<vector<pair<int, int>>> t(n);
+  vector<int> p(n);
+  for (int i = 0; i < n; i++) {
+    g.clear_flow();
+    int f = g.max_flow(i, p[i]);
+    vector<bool> cut = g.min_cut();
+    for (int j = i + 1; j < n; j++) {
+      if (cut[j] == cut[i] && p[j] == p[i]) {
+        p[j] = i;
+      }
+    }
+    t[p[i]].emplace_back(i, f);
+  }
+  return t;
+}
