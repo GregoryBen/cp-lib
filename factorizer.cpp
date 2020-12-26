2db5de4e738aba72af3c11bf7d4cfaeded547e5b
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2020-12-26 23:35:04 +0700

    Update factorizer.cpp

--- factorizer.cpp
+++ factorizer.cpp
@@ -2,15 +2,13 @@
 // 7 times slower than a^b mod m / O(n^{1/4}) gcd calls
 // source : https://github.com/FlowerOfSorrow/Cpp_Implementations/blob/master/Numeric/Number_Theory/pollard_rho.sublime-snippet
 
-using ull = unsigned long long;
-
-ull mult(ull a, ull b, ull md) {
-  long long ret = a * b - md * ull(1.L / md * a * b);
+long long mult(long long a, long long b, long long md) {
+  long long ret = a * b - md * (long long) (1.L / md * a * b);
   return ret + md * (ret < 0) - md * (ret >= (long long) md);
 }
 
-ull power(ull a, ull b, ull md) {
-  ull ans = 1;
+long long power(long long a, long long b, long long md) {
+  long long ans = 1;
   while (b > 0) {
     if (b & 1) {
       ans = mult(ans, a, md);
@@ -21,53 +19,59 @@ ull power(ull a, ull b, ull md) {
   return ans;
 }
 
-bool is_prime(ull n) {
+bool is_prime(long long n) {
   if (n < 2 || n % 6 % 4 != 1) {
     return (n | 1) == 3;
   }
-  ull s = __builtin_ctzll(n - 1);
-  ull d = n >> s;
-  const ull A[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-  for (ull a : A) {
-    ull p = power(a % n, d, n), i = s;
+  long long s = __builtin_ctzll(n - 1);
+  long long d = n >> s;
+  long long A[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  for (long long a : A) {
+    long long p = power(a % n, d, n), i = s;
     while (p != 1 && p != n - 1 && a % n && i--) {
       p = mult(p, p, n);
     }
-    if (p != n - 1 && i != s) {
+    if (p != n-1 && i != s) {
       return false;
     }
   }
   return true;
 }
 
-ull pollard(ull n) {
-  auto f = [n](ull x) {
+long long pollard(unsigned long long n) {
+  auto f = [n](long long x) {
     return mult(x, x, n) + 1;
   };
-  ull x = 0, y = 0, t = 0, prd = 2, i = 1, q;
-  while (t++ % 40 || gcd(prd, n) == 1) {
+  unsigned long long x = 0, y = 0, t = 0;
+  unsigned long long prd = 2, i = 1, q;
+  while (t++ % 40 || __gcd(prd, n) == 1) {
     if (x == y) {
-      x = ++i;
+      ++i;
+      x = i;
       y = f(x);
     }
-    if ((q = mult(prd, max(x,y) - min(x,y), n))) {
+    q = mult(prd, max(x, y) - min(x, y), n);
+    if (q > 0) {
       prd = q;
     }
-    x = f(x), y = f(f(y));
+    x = f(x);
+    y = f(f(y));
   }
-  return gcd(prd, n);
+  return __gcd(prd, n);
 }
 
-vector<ull> factor(ull n) {
+vector<long long> factor(long long n) {
+  assert(n > 0);
   if (n == 1) {
-    return vector<ull>();
+    return vector<long long>();
   }
   if (is_prime(n)) {
     return {n};
   }
-  ull x = pollard(n);
-  vector<ull> l = factor(x);
-  vector<ull> r = factor(n / x);
+  long long x = pollard(n);
+  vector<long long> l = factor(x);
+  vector<long long> r = factor(n / x);
   l.insert(l.end(), r.begin() ,r.end());
   return l;
 }
+
