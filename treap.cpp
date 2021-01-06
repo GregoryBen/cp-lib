2676a2e394dcc9865b16b00e4afd6bb16b33100a
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2021-01-06 22:28:06 +0700

    Update treap.cpp

--- treap.cpp
+++ treap.cpp
@@ -1,7 +1,7 @@
-// simple treap (not perfict)
+// simple treap (not perfect)
 // need rng
 // https://github.com/indy256/codelibrary/blob/master/cpp/structures/treap.h (update range, query range, apply)
-// https://github.com/ADJA/algos/blob/master/DataStructures/CartesianTreeImplicitKeys.cpp
+// https://github.com/ADJA/algos/blob/master/DataStructures/CartesianTreeImplicitKeys.cpp (query range, no update)
 // function split and merge output treap
 
 struct tnode {
